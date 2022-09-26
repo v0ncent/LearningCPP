49ba60c7ce022e142275a7c92f9e16a675b38aef
v0ncent <83373495+v0ncent@users.noreply.github.com>
2022-09-26 02:45:49 -0500

    Added more notes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 //Headers
 #include <iostream> //headers are what these are called, and they are basically import statements
+#include <vector>
 //Every c++ program will probably include iostream it is the basic library for c++
 
 //Namespaces
 using namespace std; //in c++ we can use namespaces for libraries to call methods from them directly
+
+
+//Functions
+//syntax: returnType functionName(parameters){
+//      code
+//}
+//one unique thing about cpp functions is you can declare default values to parameters
+void defaultTypeParameter(std::string param = "default") {
+    cout << param;
+}
 
 //Main Function
 // this is the main function for c++ and is globally scoped for the compiler
@@ -36,9 +47,30 @@ int main() {
     //User Input
     //Basic example of user input
     string input;
-    cout << "Say something!" << endl;
+    cout << "Say something!\n";
     cin >> input;
-    cout << "You said: " << input;
+    cout << "You said: " << input + "\n";
+
+    //Data types
+    int integerType = 0; //int same as java
+    double doubleType = 1.2; //decimal same as java
+    char charType = 's'; // char same as java
+    bool boolType = false; //bool basically the same as java just bool not boolean
+    string strType = "Example String";
+    // you can declare constants like so in c++
+    //const string constant = "Constant";
+
+    //Arrays in c++
+    //arrays are declared like so
+    string stringArray[3] = {"String1", "String2", "String3"};
+    // can change index values the same as java
+    // loop through array like this
+    for (auto & i : stringArray) {
+        cout << i << " \n";
+    }
+
+    //Call functions
+    defaultTypeParameter();
 
     //Why return 0 in main function
     return 0; //returning zero means the program ran successfully but is optional to include
